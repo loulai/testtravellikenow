6c97859c6b8d5462a834c1f6924f79208492b239
loulai <louiselai88@gmail.com>
2015-08-16 23:08:56 -0400

    up to creating links in intro

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,21 @@
 class PlacesController < ApplicationController
+	def index
+		@places = Place.all
+	end
+
+	def create
+		@place = Place.new(place_params)
+
+		@place.save
+		redirect_to @place
+	end
+
+	def show
+		@place = Place.find(params[:id])
+	end
+
+	private
+		def place_params
+			params.require(:place).permit(:title)
+		end
 end
